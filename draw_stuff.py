7d97e8e48a90a3e38c380ef33f5dfaaae963e07f
Heinle <jheinl3766@greenville.k12.sc.us>
2017-10-18 09:28:15 -0400

    added a draw thingy

---
+++ draw_stuff.py
@@ -0,0 +1,139 @@
+from random import randint
+from turtle import *
+import math
+i = True
+pensize(5)
+rt = Turtle()
+again = True
+screensize(1000,600)
+colormode(255)
+radians()
+
+
+def triangle():
+    for r in range(3):
+        left(2 * math.pi / 3)
+        col = 1
+        for i in range(13):
+            if col == 1:
+                color(0, 0, 0)
+                forward(20)
+                col = -1
+            else:
+                penup()
+                forward(20)
+                pendown()
+                col = 1
+
+def square():
+    for r in range(4):
+        left(math.pi / 2)
+        col = 1
+        for i in range(13):
+            if col == 1:
+                color(0, 0, 0)
+                forward(20)
+                col = -1
+            else:
+                penup()
+                forward(20)
+                pendown()
+                col = 1
+                
+def squigly():
+    leftr = 0
+    rightr = 0
+    leftrrt = 0
+    rightrrt = 0
+    for r in range(1):
+        rt.pensize(5)
+        
+        speed(10)
+        for r in range(2):
+            for i in range(20):
+                lr = randint(0,1)
+                
+                if lr == 0:
+                    for r in range(6):
+                        if r >= 3:
+                            leftr = leftr + .1
+                            leftrrt = leftrrt + .1
+                            color(randint(1,255), randint(1,255), randint(1,255))
+                            left(leftr)
+                            forward(5)
+                            rt.left(leftrrt)
+                            rt.forward(5)
+                        else:
+                            leftr = leftr + .1
+                            leftrrt = leftrrt + .1
+                            left(leftr)
+                            penup()
+                            forward(5)
+                            pendown()
+                            rt.left(leftrrt)
+                            rt.penup()
+                            rt.forward(5)
+                            rt.pendown()
+
+                if lr > 0:
+                    for r in range(6):
+                        if r >= 3:
+                            rightr = rightr + .1
+                            rightrrt = rightrrt + .1
+                            color(randint(1,255), randint(1,255), randint(1,255))
+                            right(rightr)
+                            forward(5)
+                            rt.right(rightrrt)
+                            rt.forward(5)
+                        else:
+                            rightr = rightr + .1
+                            rightrrt = rightrrt + .1
+                            right(rightr)
+                            penup()
+                            forward(5)
+                            pendown()
+                            rt.right(rightrrt)
+                            rt.penup()
+                            rt.forward(5)
+                            rt.pendown()
+            penup()
+            home()
+            pendown()
+            rt.penup()
+            rt.home()
+            rt.pendown()
+        rt.clear()
+        clear()
+
+def l():
+    left(.5)
+
+def r():
+    right(.5)
+    
+def game():
+    la = True
+    while la == True:
+        forward(1)
+        onkeyrelease(l, "a")
+        onkeyrelease(r, "d")
+        listen()
+
+def timer():
+    time = 0
+    while time != 300:
+        time = time + 1
+        print(time)
+    clear()
+    
+while again == True:
+    triangle()
+    timer()
+    square()
+    timer()
+    squigly()
+    timer()
+    game()
+    timer()
+
+
